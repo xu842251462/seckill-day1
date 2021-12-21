15221e205fece610ef76f780bacb6056cd5cff11
binn xuu <xu842251462@outlook.com>
2021-12-21 00:02:24 -0500

    Initial commit: missing maven, redis installation, mybatis generator code

---
+++ src/main/java/com/jiuzhang/seckill/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+//package com.jiuzhang.seckill.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("echo")
+//public class HelloWorldController {
+//
+//    @GetMapping
+//    public String helloWorld() {
+//        return "Hello World";
+//    }
+//}
